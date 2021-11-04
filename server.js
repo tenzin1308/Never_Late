011995513aa5501e34fe941559efc53ab5f74992
tenzin1308 <56772873+tenzin1308@users.noreply.github.com>
2021-11-03 23:34:23 -0400

    production 5

--- server.js
+++ server.js
@@ -40,7 +40,8 @@ if (process.env.NODE_ENV === "production") {
     })
 }
 // 
-app.listen(process.env.PORT || 5000, () => {
+const port = process.env.PORT || 5000
+app.listen(port, () => {
     console.log(`Server started at http://localhost:${port}`);
 })
 
